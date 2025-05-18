33d9c971516d7e58d057fdd018a3847b0bab9e75
AstrakhantsevaNastya <anastasia.astr1904@gmail.com>
2025-05-18 17:51:51 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -4,20 +4,19 @@
 
 class Train {
 private:
-    struct Car {
-        bool light; // состояние лампочки
-        Car* next; // следующий вагон
-        Car* prev; // предыдущий вагон
-    };
-
-    int operationCount; // счетчик операций
-    Car* firstCar; // первый вагон поезда
+ struct Car {
+   bool light; // состояние лампочки
+   Car* next; // следующий вагон
+   Car* prev; // предыдущий вагон
+ };
+ int operationCount; // счетчик операций
+ Car* firstCar; // первый вагон поезда
 
 public:
-    Train();
-    void addCar(bool light); // добавить вагон
-    int getLength(); // вычислить длину поезда
-    int getOpCount() const;  // вернуть число операций
+ Train();
+ void addCar(bool light); // добавить вагон
+ int getLength(); // вычислить длину поезда
+ int getOpCount() const;  // вернуть число операций
 };
 
 #endif  // INCLUDE_TRAIN_H_
